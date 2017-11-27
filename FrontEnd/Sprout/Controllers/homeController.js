bae97e9e9e090366448130360a3355fb4024796b
Nico Disera <u0869073@utah.edu>
2017-11-27 14:24:33 -0700

    Added login check to other controllers.

--- FrontEnd/Sprout/Controllers/homeController.js
+++ FrontEnd/Sprout/Controllers/homeController.js
@@ -1,3 +1,5 @@
 ﻿app.controller("homeController", function ($scope, $location) {
-
+    if (!JSON.parse(localStorage.getItem("loggedIn"))) {
+        location.path('');
+    }
 });
--- FrontEnd/Sprout/Controllers/scoresInputController.js
+++ FrontEnd/Sprout/Controllers/scoresInputController.js
@@ -1,1 +1,5 @@
-﻿
+﻿app.controller("scoresInputController", function ($scope, $location) {
+    if (!JSON.parse(localStorage.getItem("loggedIn"))) {
+        location.path('');
+    }
+});
