52f46bd8467340153d579b4d9a6e81c398de5ce5
Nico Disera <u0869073@utah.edu>
2017-11-28 22:10:47 -0700

    Pointer now appears when hovering over tabs. Have behavior form that clears inputs upon date submission, definitely has a lot of work left.

--- FrontEnd/Sprout/Controllers/studentController.js
+++ FrontEnd/Sprout/Controllers/studentController.js
@@ -4,6 +4,47 @@
     }
 
     $('#datepicker').datepicker({
-        format: "dd/mm/yyyy"
+        format: "mm/dd/yyyy"
     });
+
+    $scope.records = [
+    {
+        "Name": "Math",
+        "Behavior": "",
+        "Effort": ""
+    },
+    {
+        "Name": "Reading",
+        "Behavior": "",
+        "Effort": ""
+    },
+    {
+        "Name": "Art",
+        "Behavior": "",
+        "Effort": ""
+    },
+    {
+        "Name": "Science",
+        "Behavior": "",
+        "Effort": ""
+    },
+    {
+        "Name": "History",
+        "Behavior": "",
+        "Effort": ""
+    },
+    {
+        "Name": "English",
+        "Behavior": "",
+        "Effort": ""
+    }
+    ];
+
+    $scope.changeDate = function (event) {
+        event.preventDefault();
+        // set select boxes back to one
+        var temp = "";
+        $(".effortScore").val("");
+        $(".behaviorScore").val("");
+    }
 });
