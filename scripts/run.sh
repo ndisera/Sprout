91488e2e47b3cdc1c574356002ed60f3009014d3
Simon Redman <simon@ergotech.com>
2017-11-30 22:27:11 -0700

    Add signal handling to run.sh
    fixes #10

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,21 @@
 # Source python environment
 source ../server/env/bin/activate
 
+# Setup a signal handler
+trap killgroup SIGINT
+
+killgroup()
+{
+  # If we recieve ctrl-c, kill ourselves
+  kill 0
+}
+
 # run server and client in parallel
+node ../client/server.js -f public -p 8001 &
+client=$!
+
 python ../server/manage.py runserver &
 server=$!
-
-node ../client/server.js -f public -p 8001
-client=$!
 
 wait $server $client
 
