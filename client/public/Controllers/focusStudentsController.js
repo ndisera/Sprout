9847113eade41208d45f3c51fbfc647baa7be3e0
Nico Disera <u0869073@utah.edu>
2017-12-04 18:41:05 -0700

    Changed look of focus students layout, got rid of inline styling, included and used jqueryUI (cdn) sortable.

--- client/public/Controllers/focusStudentsController.js
+++ client/public/Controllers/focusStudentsController.js
@@ -1,5 +1,23 @@
-﻿app.controller("fsController", function ($scope, $location) {
+﻿app.controller("focusStudentsController", function ($scope, $location) {
     if (!JSON.parse(localStorage.getItem("loggedIn"))) {
         location.path('');
     }
+
+    // draggable 
+    $('.row').sortable({
+        connectWith: ".panel",
+        handle: ".panel-heading",
+        placeholder: "panel-placeholder",
+        start: function (e, ui) {
+            ui.placeholder.width(ui.item.find('.panel').width());
+            ui.placeholder.height(ui.item.find('.panel').height());
+            ui.placeholder.addClass(ui.item.attr("class"));
+        }
+    });
+
+    $('.panel').on('mousedown', function () {
+        $(this).css('cursor', 'move');
+    }).on('mouseup', function () {
+        $(this).css('cursor', 'auto');
+    });
 });
