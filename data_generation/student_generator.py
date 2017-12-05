4b8d2c681fae3ea5b50229f3d5668f4418472a49
Simon Redman <simon@ergotech.com>
2017-12-04 22:41:27 -0700

    Update student_generator for latest database schema

--- data_generation/student_generator.py
+++ data_generation/student_generator.py
@@ -10,10 +10,12 @@ class StudentGenerator(object):
     scripts can do anything
     """
 
+    RANDOM_STUDENT_ID_PREFIX = "gen"
+
     def __init__(self, url="http://localhost", port_num=8000):
         self.url = url
         self.port_num = port_num
-        self.url_port = str(self.url) + ":" + str(self.port_num) + "/student/"
+        self.complete_uri = str(self.url) + ":" + str(self.port_num) + "/students/"
 
     def upload_many_random_students(self,
                                     num_students,
@@ -53,9 +55,9 @@ class StudentGenerator(object):
                               birthdate_range_end=datetime.date(2008, 9, 1)):
         """Generate and upload a random student based on the passed parameters
 
-        :param first_names:
+        :param first_names: List from which to select first names
         :type first_names: list of strings
-        :param last_names:
+        :param last_names: List from which to select last names
         :type last_names: list of strings
         :param birthdate_range_start:
         :type birthdate_range_start: datetime.date
@@ -66,35 +68,44 @@ class StudentGenerator(object):
         first_name = random.choice(first_names)
         last_name = random.choice(last_names)
 
+        # Generate a random birthday
         date_range_size = birthdate_range_end - birthdate_range_start
-
         random_date_delta = random.randint(0, date_range_size.days)
-
         birthdate = birthdate_range_start + datetime.timedelta(random_date_delta)
 
-        self.upload_student(first_name, last_name, birthdate)
+        # Generate a random student id
+        random_id_number = random.randrange(0, 999999)
+        # If that number turns out to be non-unique, we will be in trouble. Cross your fingers!
+        student_id = str(StudentGenerator.RANDOM_STUDENT_ID_PREFIX + str(random_id_number))
 
+        self.upload_student(student_id, first_name, last_name, birthdate)
 
     def upload_developer_information(self):
-        self.upload_student(first_name="Nico",
+        self.upload_student(student_id="dev960520",
+                            first_name="Nico",
                             last_name="DiSera",
                             birthdate=datetime.date(year=1996, month=5, day=20))
 
-        self.upload_student(first_name="Simon",
+        self.upload_student(student_id="dev950118",
+                            first_name="Simon",
                             last_name="Redman",
                             birthdate=datetime.date(year=1995, month=1, day=18))
 
-        self.upload_student(first_name="Guy",
+        self.upload_student(student_id="dev520807",
+                            first_name="Guy",
                             last_name="Watson",
                             birthdate=datetime.date(year=1752, month=8, day=7))
 
-        self.upload_student(first_name="Graham",
+        self.upload_student(student_id="dev890918",
+                            first_name="Graham",
                             last_name="Zuber",
                             birthdate=datetime.date(year=1989, month=9, day=18))
 
-    def upload_student(self, first_name, last_name, birthdate):
+    def upload_student(self, student_id, first_name, last_name, birthdate):
         """Upload a single student to the server
 
+        :param student_id: The unique identifier for this student
+        :type student_id: str
         :param first_name: First name of the student
         :type first_name: str
         :param last_name: Last name of the student
@@ -103,10 +114,11 @@ class StudentGenerator(object):
         :type birthdate: datetime.date
         :return:
         """
-        json = {"first_name":first_name,
+        json = {"student_id": student_id,
+                "first_name":first_name,
                 "last_name":last_name,
                 "birthdate":str(birthdate)}
-        response = requests.post(url=self.url_port, json=json)
+        response = requests.post(url=self.complete_uri, json=json)
 
         if response.status_code >= 400 and response.status_code < 500:
             raise "Unable to POST student: " + str(json)
@@ -114,4 +126,5 @@ class StudentGenerator(object):
 
 if __name__ == "__main__":
     generator = StudentGenerator()
+    #generator.upload_developer_information();
     generator.upload_many_random_students(5)
