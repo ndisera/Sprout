1ff4cb55f86e70e598b1e7f760e593c6643495f6
Simon Redman <simon@ergotech.com>
2017-12-06 12:56:03 -0700

    Add tests for student_id

--- server/tests/test_student_api.py
+++ server/tests/test_student_api.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
+from django.db import IntegrityError
 from api.models import Student
 
 import datetime
 
 class StudentTestCase(TestCase):
     def setUp(self):
-        Student.objects.create(first_name="Simon", last_name="Redman", birthdate="1995-01-18")
+        Student.objects.create(student_id="dev950118",
+                               first_name="Simon",
+                               last_name="Redman",
+                               birthdate="1995-01-18")
 
     def test_student_exists(self):
         simon = Student.objects.get(first_name="Simon")
@@ -29,3 +33,17 @@ class StudentTestCase(TestCase):
         self.assertEqual(simon.birthdate,
                          datetime.date(year=1995, month=1, day=18),
                          "Incorrect birthday returned")
+
+    def test_student_id(self):
+        simon = Student.objects.get(first_name="Simon")
+        self.assertEqual(simon.student_id,
+                         "dev950118",
+                         "Incorrect student_id returned")
+
+    def test_student_id_uniqueness(self):
+        # This test is expected to raise an error because student_id is not unique
+        with self.assertRaises(IntegrityError):
+            Student.objects.create(student_id="dev950118",
+                                   first_name="Evil",
+                                   last_name="Twin",
+                                   birthdate="1995-01-18")
