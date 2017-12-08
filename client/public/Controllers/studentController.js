b43865d6bc7bdc00b9f8a0fe84af90f8467b60d6
Simon Redman <simon@ergotech.com>
2017-12-07 21:06:22 -0700

    Restore Guy's basically hard-coded bar charts

--- client/public/Controllers/studentController.js
+++ client/public/Controllers/studentController.js
@@ -1,5 +1,6 @@
 ﻿app.controller("studentController", function ($scope, $location, $http, $rootScope, $routeParams) {
-
+  
+    $scope.class_titles = [];
     $scope.classes = [];
     $scope.behaviors = [];
 
@@ -22,6 +23,9 @@
                     id: response.data.id,
                     title: response.data.title
                 });
+                $scope.class_titles.push(
+                  response.data.title
+                );
                 var x = $scope.classes;
             }, function errorCallback(response) {
                 $scope.status = response.status;
@@ -100,43 +104,25 @@
         $scope.changeDate();
     });
 
-    var ctx = document.getElementById("student_behavior").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
+    $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40]
+    ];
+    $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
+    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
+    $scope.options = {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
         }
-    });
+        ]
+      }
+    };
 });
