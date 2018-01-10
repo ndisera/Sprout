17bcb1d27ed1d33e76b0663dfc2ab322ee676213
Simon Redman <simon@ergotech.com>
2018-01-09 19:42:27 -0700

    Add check for npm to be installed in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,13 @@ if [ ! $? -eq 0 ]; then
   exit 1
 fi
 
+# Check if npm is installed
+which npm &>/dev/null
+if [ ! $? -eq 0 ]; then
+  echo "Error: npm is not on the PATH. Aborting."
+  exit 1
+fi
+
 # set up the virtualenv
 cd ../server
 virtualenv env
