189a390337465c677deccfc9132b8d01cd060bd5
Graham Zuber <grahamzuber@gmail.com>
2018-01-25 00:57:37 -0700

    added queryService to build query strings for requests.

---
+++ client/src/Services/queryService.js
@@ -0,0 +1,56 @@
+app.factory("queryService", function () {
+
+    function createInclude(elem) {
+        return 'include[]=' + elem + '&';
+    }
+
+    function createExclude(elem) {
+        return 'exclude[]=' + elem + '&';
+    }
+
+    function createFilter(elem) {
+        return 'filter{' + elem.name + '}=' + elem.val + '&';
+    }
+    
+    function createSort(elem) {
+        return 'sort[]=' + elem + '&';
+    }
+
+    function reduceQuery(memo, val) {
+        return memo + val;
+    }
+    
+    return {
+        /**
+         * Create a url query string
+         * @param {object} config - all query params to be applied
+         *
+         * example config
+         * {
+         *      include: [this],
+         *      exclude: [that],
+         *      filter: [
+         *          { name: date.range, val: 2017-01-20 },
+         *          { name: date.range, val: 2017-01-24 },
+         *      ],
+         *      sort: [date],
+         * }
+         *
+         * produces:
+         *
+         * ?include[]=this&exclude[]=that&filter{date.range}=2017-01-20   \
+         * &filter{date.range}=2017-01-24&sort[]=date
+         */
+        generateQuery: function (config) {
+            var queries = [];
+            queries.push(_.map(config.include, createInclude));
+            queries.push(_.map(config.exclude, createExclude));
+            queries.push(_.map(config.filter, createFilter));
+            queries.push(_.map(config.sort, createSort));
+            console.log(queries);
+            var queries_flat = _.flatten(queries);
+
+            return _.reduce(queries_flat, reduceQuery, '?').slice(0, -1);
+        },
+    };
+});
