72725e646617bf842ea630ab3a6bc98ae90b39bb
Nico Disera <u0869073@utah.edu>
2018-01-28 19:34:59 -0700

    Added the standard methods for the enrollment and section services.

--- client/src/Services/enrollmentService.js
+++ client/src/Services/enrollmentService.js
@@ -17,6 +17,63 @@ app.factory("enrollmentService", function ($rootScope, $http, $q, queryService) 
                 deferred.reject(response);
             });
             return deferred.promise;
+        },
+
+        /**
+         * Add student section enrollment
+         * @param {object} enrollmentObj - object to be added to enrollments.
+         * @return {promise} promise that will resolve with data or reject with response code.
+         */
+        addStudentEnrollment: function (enrollmentObj) {
+            var deferred = $q.defer();
+            $http({
+                method: 'POST',
+                url: 'https://' + $rootScope.backend + '/enrollments',
+                data: enrollmentObj
+            }).then(function success(response) {
+                deferred.resolve(response.data);
+            }, function error(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        },
+
+        /**
+         * Update student section enrollment
+         * @param {number} enrollmentId - the enrollment id.
+         * @param {object} enrolmentObj - the object used to update the enrollment.
+         * @return {promise} promise that will resolve with data or reject with response code.
+         */
+        updateStudentEnrollment: function (enrollmentId, enrollmentObj) {
+            var deferred = $q.defer();
+            $http({
+                method: 'UPDATE',
+                url: 'https://' + $rootScope.backend + '/enrollments/' + enrollmentId,
+                data: enrollmentObj
+            }).then(function success(response) {
+                deferred.resolve(response.data);
+            }, function error(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        },
+
+        /**
+         * Delete student section enrollment
+         * @param {number} enrollmentId - the enrollment id.
+         * @return {promise} promise that will resolve with data or reject with response code.
+         */
+        deleteStudentEnrollment: function (enrollmentId) {
+            var deferred = $q.defer();
+            $http({
+                method: 'DELETE',
+                url: 'https://' + $rootScope.backend + '/enrollments/' + enrollmentId
+            }).then(function success(response) {
+                deferred.resolve(response.data);
+            }, function error(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
         }
     };
 });
--- client/src/Services/sectionService.js
+++ client/src/Services/sectionService.js
@@ -18,6 +18,63 @@
                 deferred.reject(response);
             });
             return deferred.promise;
+        },
+
+        /**
+         * Add section
+         * @param {object} sectionObj - object to be added.
+         * @return {promise} promise that will resolve with data or reject with response code.
+         */
+        addSection: function (sectionObj) {
+            var deferred = $q.defer();
+            $http({
+                method: 'POST',
+                url: 'https://' + $rootScope.backend + '/sections/',
+                data: sectionObj
+            }).then(function success(response) {
+                deferred.resolve(response.data);
+            }, function error(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        },
+
+        /**
+         * Update section
+         * @param {number} sectionId - the section id.
+         * @param {object} sectionObj - object used to update.
+         * @return {promise} promise that will resolve with data or reject with response code.
+         */
+        updateSection: function (sectionId, sectionObj) {
+            var deferred = $q.defer();
+            $http({
+                method: 'PUT',
+                url: 'https://' + $rootScope.backend + '/sections/' + sectionId,
+                data: sectionObj
+            }).then(function success(response) {
+                deferred.resolve(response.data);
+            }, function error(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        },
+
+        /**
+         * Delete section
+         * @param {number} sectionId - the section id.
+         * @return {promise} promise that will resolve with data or reject with response code.
+         */
+        deleteSection: function (sectionId) {
+            var deferred = $q.defer();
+            $http({
+                method: 'DELETE',
+                url: 'https://' + $rootScope.backend + '/sections/' + sectionId
+            }).then(function success(response) {
+                deferred.resolve(response.data);
+            }, function error(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
         }
     };
 });
