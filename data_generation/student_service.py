12adee36ca4ca5a6bf6face44846fd65091a3cae
Simon Redman <simon@ergotech.com>
2018-02-01 18:53:54 -0700

    Add helper service for handling students

---
+++ data_generation/student_service.py
@@ -0,0 +1,54 @@
+
+import requests
+
+from authorization_handler import CERT_PATH
+from collections import namedtuple
+
+Student = namedtuple("Student", ['student_id', 'first_name', 'last_name', 'birthdate'])
+
+
+class GradesService():
+
+    def __init__(self, headers={}, url="localhost", port_num=8000, verify=CERT_PATH):
+        self.headers = headers
+        self.url = url
+        self.port_num = port_num
+        self.complete_uri = "https://" + str(self.url) + ":" + str(self.port_num) + "/students/"
+        self.verify = verify
+
+    def get_students(self):
+        """
+        Download a complete list of students
+
+        :return: list of student objects
+        :rtype: list[Student]
+        """
+        response = requests.get(self.complete_uri, verify=self.verify, headers=self.headers)
+
+        if response.status_code >= 200 and response.status_code < 299:
+            response.raise_for_status()
+
+        body = response.json()
+        students = body['students']
+
+        toReturn = []
+
+        for student in students:
+            toReturn.append(Student(student_id=student['student_id'],
+                                    first_name=student['first_name'],
+                                    last_name=student['last_name'],
+                                    birthdate=student['birthdate']))
+
+        return toReturn
+
+    def add_student(self, student):
+        """
+        Upload a student object to the server
+
+        :param student: Student object to upload
+        :type student: Student
+        """
+        response = requests.post(self.complete_uri, verify=self.verify, headers=self.headers, data=student._asdict())
+
+        if response.status_code >= 200 and response.status_code < 299:
+            response.raise_for_status()
