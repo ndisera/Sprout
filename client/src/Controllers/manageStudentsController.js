10b0e42fce13fa329aba0cc572bde955418613c3
Nico Disera <u0869073@utah.edu>
2018-02-06 01:53:34 -0700

    Added functionality for enrolling and removing students from a section.

--- client/src/Controllers/manageStudentsController.js
+++ client/src/Controllers/manageStudentsController.js
@@ -1,4 +1,5 @@
-﻿app.controller("manageStudentsController", function ($scope, $rootScope, $location, students, studentService) {
+﻿
+app.controller("manageStudentsController", function($scope, $rootScope, $location, students, studentService) {
 
     var studentTask = "view/edit";
     var studentDSearchOrInfo = "search";
@@ -16,7 +17,7 @@
      * Display search or form depending on the student task selected and set the active button
      * @param {string} task - the type of task selected.
      */
-    $scope.changeStudentTask = function (task) {
+    $scope.changeStudentTask = function(task) {
         switch (studentTask) {
             case "view/edit":
                 $scope.displayStudentViewSearch = false;
@@ -57,12 +58,11 @@
     /**
      * Navigates to student's page if name in navigation search bar is valid.
      */
-    $scope.viewStudent = function () {
+    $scope.viewStudent = function() {
         if ($scope.studentViewSearch.toUpperCase() in $scope.studentInfo.studentsLookup) {
             $location.path('/student/' + $scope.studentInfo.studentsLookup[$scope.studentViewSearch.toUpperCase()].id);
             return;
-        }
-        else {
+        } else {
             //TODO: notify the user in some way
         }
     };
@@ -70,18 +70,18 @@
     /**
      * Creates and adds a new student.
      */
-    $scope.addStudent = function () {
+    $scope.addStudent = function() {
         var studentPromise = studentService.addStudent($scope.newStudent);
         studentPromise.then(function success(data) {
             $scope.newStudent = {};
             $scope.addStudentSuccess = true;
-            $("#addStudentSuccess").fadeTo(2000, 500).slideUp(500, function () {
+            $("#addStudentSuccess").fadeTo(2000, 500).slideUp(500, function() {
                 $("#addStudentSuccess").slideUp(500);
             });
         }, function error(response) {
             setErrorMessage(response);
             $scope.addStudentFailure = true;
-            $("#addStudentFailure").fadeTo(5000, 500).slideUp(500, function () {
+            $("#addStudentFailure").fadeTo(5000, 500).slideUp(500, function() {
                 $("#addStudentFailure").slideUp(500);
             });
         });
@@ -90,7 +90,7 @@
     /**
      * Hides the delete student search bar and displays their info with an option to delete.
      */
-    $scope.displayDeleteStudent = function () {
+    $scope.displayDeleteStudent = function() {
         if ($scope.studentDeleteSearch.toUpperCase() in $scope.studentInfo.studentsLookup) {
             $scope.studentD = $scope.studentInfo.studentsLookup[$scope.studentDeleteSearch.toUpperCase()];
             $scope.displayStudentDeleteSearch = false;
@@ -103,12 +103,12 @@
     /**
      * Deletes the selected student from the database.
      */
-    $scope.deleteStudent = function () {
+    $scope.deleteStudent = function() {
         var studentPromise = studentService.deleteStudent($scope.studentD.id);
         studentPromise.then(function success(data) {
             $scope.studentD = {};
             $scope.deleteStudentSuccess = true;
-            $("#deleteStudentSuccess").fadeTo(2000, 500).slideUp(500, function () {
+            $("#deleteStudentSuccess").fadeTo(2000, 500).slideUp(500, function() {
                 $("#deleteStudentSuccess").slideUp(500);
             });
             $scope.displayStudentDeleteSearch = true;
@@ -118,7 +118,7 @@
         }, function error(response) {
             setErrorMessage(response);
             $scope.deleteStudentFailure = true;
-            $("#deleteStudentFailure").fadeTo(5000, 500).slideUp(500, function () {
+            $("#deleteStudentFailure").fadeTo(5000, 500).slideUp(500, function() {
                 $("#deleteStudentFailure").slideUp(500);
             });
         });
@@ -127,7 +127,7 @@
     /**
      * Restores the delete student search box and hides their info and delete option.
      */
-    $scope.cancelDeleteStudent = function () {
+    $scope.cancelDeleteStudent = function() {
         $scope.clearStudentDeleteSearch();
         $scope.displayStudentDeleteSearch = true;
         $scope.displayStudentInfo = false;
@@ -138,14 +138,14 @@
     /**
      * Clears the view student search bar.
      */
-    $scope.clearStudentViewSearch = function () {
+    $scope.clearStudentViewSearch = function() {
         $scope.studentViewSearch = "";
     };
 
     /**
      * Clears the delete student search bar.
      */
-    $scope.clearStudentDeleteSearch = function () {
+    $scope.clearStudentDeleteSearch = function() {
         $scope.studentDeleteSearch = "";
     };
 
@@ -177,6 +177,10 @@
         }
     }
 
+    /**
+     * Updates the displayed error message.
+     * @param {response} response - response containing data and error message.
+     */
     function setErrorMessage(response) {
         $scope.errorMessage = [];
         for (var property in response.data) {
