87c9df48d5e7f3568e4e3a046d3e14a359b67bbc
Simon Redman <simon@ergotech.com>
2018-02-17 19:47:14 -0700

    Remove setup from student_generator.py

--- data_generation/student_generator.py
+++ data_generation/student_generator.py
@@ -12,8 +12,6 @@ from authorization_service import CERT_PATH
 
 from student_service import Student, StudentService
 
-headers = { }
-
 class StudentGenerator(object):
     """StudentGenerator contains various methods for generating student data
 
@@ -33,13 +31,13 @@ class StudentGenerator(object):
 
         self.studentService = StudentService(headers=headers, url=url, port_num=port_num, verify=verify)
 
-    def upload_many_random_students(self,
-                                    num_students,
-                                    first_names_file="./first_names.txt",
-                                    last_names_file="./last_names.txt",
-                                    birthdate_range_start=datetime.date(2006, 9, 1),
-                                    birthdate_range_end=datetime.date(2008, 9, 1)):
-        """Upload a specified number of students by reading first and last names from text files
+    def generate_many_random_students(self,
+                                      num_students,
+                                      first_names_file="./first_names.txt",
+                                      last_names_file="./last_names.txt",
+                                      birthdate_range_start=datetime.date(2006, 9, 1),
+                                      birthdate_range_end=datetime.date(2008, 9, 1)):
+        """Generate a specified number of students by reading first and last names from text files
 
         :param num_students: Number of students to upload
         :type num_students: int
@@ -49,8 +47,13 @@ class StudentGenerator(object):
         :type birthdate_range_start: datetime.date
         :param birthdate_range_end:
         :type birthdate_range_end: datetime.date
-        :return: void
+        :return: list[Student]
         """
+        students = []
+
+        # Get the list of possible user IDs to use for case managers
+        case_managers = [user.id for user in self.usersService.get_users()]
+
         with open(first_names_file, 'r') as first_names_file:
             with open(last_names_file, 'r') as last_names_file:
                 first_names = first_names_file.read()
@@ -59,174 +62,70 @@ class StudentGenerator(object):
                 last_names = last_names.split()
 
                 for i in range(0, num_students):
-                    self.upload_random_student(first_names,
-                                               last_names,
-                                               birthdate_range_start,
-                                               birthdate_range_end)
+                    first_name = random.choice(first_names)
+                    last_name = random.choice(last_names)
 
-    def upload_random_student(self,
-                              first_names,
-                              last_names,
-                              birthdate_range_start=datetime.date(2006, 9, 1),
-                              birthdate_range_end=datetime.date(2008, 9, 1)):
-        """Generate and upload a random student based on the passed parameters
+                    # Generate a random birthday
+                    date_range_size = birthdate_range_end - birthdate_range_start
+                    random_date_delta = random.randint(0, date_range_size.days)
+                    birthdate = birthdate_range_start + datetime.timedelta(random_date_delta)
 
-        :param first_names: List from which to select first names
-        :type first_names: list of strings
-        :param last_names: List from which to select last names
-        :type last_names: list of strings
-        :param birthdate_range_start:
-        :type birthdate_range_start: datetime.date
-        :param birthdate_range_end:
-        :type birthdate_range_end: datetime.date
-        :return: void
+                    # Generate a random student id
+                    random_id_number = random.randrange(100000, 999999)
+                    # If that number turns out to be non-unique, we will be in trouble. Cross your fingers!
+                    student_id = str(StudentGenerator.RANDOM_STUDENT_ID_PREFIX + str(random_id_number))
+
+                    case_manager = random.choice(case_managers)
+                    student = Student(
+                        student_id=student_id,
+                        first_name=first_name,
+                        last_name=last_name,
+                        birthdate=birthdate,
+                        case_manager=case_manager,
+                    )
+                    students.append(student)
+        return students
+
+    @staticmethod
+    def generate_developer_students(case_manager_ids):
         """
-        first_name = random.choice(first_names)
-        last_name = random.choice(last_names)
-
-        # Generate a random birthday
-        date_range_size = birthdate_range_end - birthdate_range_start
-        random_date_delta = random.randint(0, date_range_size.days)
-        birthdate = birthdate_range_start + datetime.timedelta(random_date_delta)
-
-        # Generate a random student id
-        random_id_number = random.randrange(100000, 999999)
-        # If that number turns out to be non-unique, we will be in trouble. Cross your fingers!
-        student_id = str(StudentGenerator.RANDOM_STUDENT_ID_PREFIX + str(random_id_number))
-
-        self.upload_student(student_id, first_name, last_name, birthdate)
-
-    def upload_developer_information(self):
-        self.upload_student(student_id="dev960520",
-                            first_name="Nico",
-                            last_name="DiSera",
-                            birthdate=datetime.date(year=1996, month=5, day=20))
-
-        self.upload_student(student_id="dev950118",
-                            first_name="Simon",
-                            last_name="Redman",
-                            birthdate=datetime.date(year=1995, month=1, day=18))
-
-        self.upload_student(student_id="dev520807",
-                            first_name="Guy",
-                            last_name="Watson",
-                            birthdate=datetime.date(year=1752, month=8, day=7))
-
-        self.upload_student(student_id="dev890918",
-                            first_name="Graham",
-                            last_name="Zuber",
-                            birthdate=datetime.date(year=1989, month=9, day=18))
-
-    def upload_student(self, student_id, first_name, last_name, birthdate):
-        """Upload a single student to the server
-
-        :param student_id: The unique identifier for this student
-        :type student_id: str
-        :param first_name: First name of the student
-        :type first_name: str
-        :param last_name: Last name of the student
-        :type last_name: str
-        :param birthdate: Student's birthday
-        :type birthdate: datetime.date
+        Generate and upload students named after the original Sprout developers
+        :param case_manager_ids: list of valid IDs for case managers
         :return:
         """
-        student = Student(student_id=student_id,
-                          first_name=first_name,
-                          last_name=last_name,
-                          birthdate=birthdate,
-                          id=None)
-        self.studentService.add_student(student)
+        student_nico = Student(student_id="dev960520",
+                               first_name="Nico",
+                               last_name="DiSera",
+                               birthdate=str(datetime.date(year=1996, month=5, day=20)),
+                               case_manager=random.choice(case_manager_ids),
+                               id=None,
+                               )
 
-    def post_request(self, url, data):
-        response = requests.post(url=url, json=data, verify=False, headers=headers)
-        if response.status_code > 299:
-            print 'oh, crap... something went wrong. error code ' + str(response.status_code) + ' when I posted ' + url + ' with payload: ' + str(data)
-            print 'response data: ' + str(response.json())
-            sys.exit()
-        return response
+        student_simon = Student(student_id="dev950118",
+                                first_name="Simon",
+                                last_name="Redman",
+                                birthdate=str(datetime.date(year=1995, month=1, day=18)),
+                                case_manager=random.choice(case_manager_ids),
+                                id=None,
+                                )
 
+        student_guy = Student(student_id="dev520807",
+                              first_name="Guy",
+                              last_name="Watson",
+                              birthdate=str(datetime.date(year=1752, month=8, day=7)),
+                              case_manager=random.choice(case_manager_ids),
+                              id=None,
+                              )
 
-    # @staticmethod
-    def upload_basic_bitches(self):
-        # teachers
-        host = 'localhost'
-        port = 8000
-        base_url = 'https://' + host + ':' + str(port) + '/'
-        teacher_url = base_url + 'teachers/'
+        student_graham = Student(student_id="dev890918",
+                                 first_name="Graham",
+                                 last_name="Zuber",
+                                 birthdate=str(datetime.date(year=1989, month=9, day=18)),
+                                 case_manager=random.choice(case_manager_ids),
+                                 id=None,
+                                 )
 
-        teacher_matt = {
-            'username': 'mflatt',
-            'first_name': 'Matthew',
-            'last_name': 'Flatt',
-            'email': 'mflatt@totallyrealemail.edu',
-        }
-
-        teacher_danny = {
-            'username': 'dkopta',
-            'first_name': 'Daniel',
-            'last_name': 'Kopta',
-            'email': 'dkopta@totallyrealemail.edu',
-        }
-
-        response = self.post_request(url=teacher_url, data=teacher_matt)
-        teacher_matt_id = response.json()['teacher']['id']
-
-        response = self.post_request(url=teacher_url, data=teacher_danny)
-        teacher_danny_id = response.json()['teacher']['id']
-
-        # students
-        student_url = base_url + 'students/'
-
-        generator = StudentGenerator(headers=headers)
-        generator.upload_developer_information()
-
-        students = requests.get(url=student_url, verify=False, headers=headers).json()['students']
-
-        # sections
-        section_url = base_url + 'sections/'
-
-        section_matt = {
-            'teacher': teacher_matt_id,
-            'title': 'CS 5510',
-        }
-
-        section_danny = {
-            'teacher': teacher_danny_id,
-            'title': 'CS 4400',
-        }
-
-        response = self.post_request(url=section_url, data=section_matt)
-        section_matt_id = response.json()['section']['id']
-        response = self.post_request(url=section_url, data=section_danny)
-        section_danny_id = response.json()['section']['id']
-
-        # enrollments
-        enrollment_url = base_url + 'enrollments/'
-
-        enrollments = []
-        for student in students:
-            enrollments.append({'section': section_matt_id, 'student': student['id']})
-            enrollments.append({'section': section_danny_id, 'student': student['id']})
-
-        enrollments_posted = []
-        for enrollment in enrollments:
-            response = self.post_request(url=enrollment_url, data=enrollment)
-            enrollments_posted.append(response.json()['enrollment'])
-
-        # behaviors
-        behaviors_url = base_url + 'behaviors/'
-
-        current_date = datetime.date.today()
-        for day in range(1, 32):
-            for enrollment in enrollments_posted:
-                to_post = {
-                    'date': '2018-' + current_date.strftime('%m') + '-' + str(day),
-                    'enrollment': enrollment['id'],
-                    'effort': random.randint(1, 5),
-                    'behavior': random.randint(1, 5),
-                }
-                response = self.post_request(url=behaviors_url, data=to_post)
-
+        return (student_nico, student_simon, student_guy, student_graham)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Upload students and relevant information to Sprout")
@@ -240,8 +139,6 @@ if __name__ == "__main__":
                         help="login password (warning: insecure!)")
     parser.add_argument("--token", action="store", type=str,
                         help="auth token -- supersedes username and password")
-    parser.add_argument("--setup", action="store_const", default=False, const=True,
-                        help="prerelease setup script")
 
     args = parser.parse_args()
 
@@ -263,8 +160,5 @@ if __name__ == "__main__":
 
     generator = StudentGenerator(url=args.url, verify=False, headers=headers)
 
-    if args.setup:
-        generator.upload_basic_bitches()
-    else:
-        # generator.upload_developer_information();
-        generator.upload_many_random_students(5)
+    students = generator.generate_many_random_students(5)
+    generator.studentService.add_many_students(students)
