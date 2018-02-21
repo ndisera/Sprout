26909bc32144d48a01dd12ad3741971c3b4e5947
Simon Redman <simon@ergotech.com>
2018-02-21 10:29:00 -0700

    Add attempt at re-running gulp on filesystem changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,23 @@
 SCRIPT_PATH="${PWD}/$(dirname "$0")"
 
-# run gulp build
-(cd "${SCRIPT_PATH}"/../client && ./node_modules/gulp/bin/gulp.js)
-
 # Source python environment
 source "${SCRIPT_PATH}"/../server/env/bin/activate
 
+FRONTEND_SRC_DIR="${SCRIPT_PATH}"/../client
+#
+# gulp_build
+# Use gulp to assemble all of our script files
+# Arguments: None
+# Return: void
+#
+function gulp_build {
+# run gulp build
+  cd "${FRONTEND_SRC_DIR}"
+  ./node_modules/gulp/bin/gulp.js
+}
+
 # Check for dependencies
-DEPENDENCIES=( "nginx" "node" )
+DEPENDENCIES=( "nginx" "node" "inotifywait" )
 
 for dependency in "${DEPENDENCIES[@]}"; do
   which "${dependency}" &>/dev/null
@@ -25,6 +35,15 @@ killgroup()
   # If we recieve ctrl-c, kill ourselves
   kill 0
 }
+
+# Setup to run gulp on any frontend change
+gulp_build
+(
+while true; do
+  inotifywait -e close_write,moved_to,create -m "${FRONTEND_SRC_DIR}/src"
+  gulp_build
+done
+) &
 
 # run server and client in parallel
 node "${SCRIPT_PATH}"/../client/server.js -f public -p 8001 &
