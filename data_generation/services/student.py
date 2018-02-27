ea549b7949f2b2fda3e074cfbc2be9d97907a387
Simon Redman <simon@ergotech.com>
2018-02-26 23:02:46 -0700

    Rewrite StudentService to inherit BaseService

--- data_generation/services/student.py
+++ data_generation/services/student.py
@@ -1,21 +1,16 @@
 
-import json
-import requests
-
-from authorization import CERT_PATH
 from collections import namedtuple
+
+from base_service import BaseService
 
 Student = namedtuple("Student", ['student_id', 'first_name', 'last_name', 'birthdate', 'case_manager', 'id'])
 
 
-class StudentService():
+class StudentService(BaseService):
 
-    def __init__(self, headers={}, url="localhost", port_num=8000, verify=CERT_PATH):
-        self.headers = headers
-        self.url = url
-        self.port_num = port_num
-        self.complete_uri = "https://" + str(self.url) + ":" + str(self.port_num) + "/students/"
-        self.verify = verify
+    def __init__(self, **kwargs):
+        super(StudentService, self).__init__(**kwargs)
+        self.complete_uri = self.complete_uri_template.format(endpoint="/students/")
 
     def get_students(self):
         """
@@ -24,20 +19,7 @@ class StudentService():
         :return: list of student objects
         :rtype: list[Student]
         """
-        response = requests.get(self.complete_uri, verify=self.verify, headers=self.headers)
-
-        if not (response.status_code >= 200 and response.status_code < 299):
-            response.raise_for_status()
-
-        body = response.json()
-        students = body['students']
-
-        toReturn = []
-
-        for student in students:
-            toReturn.append(Student(**student))
-
-        return toReturn
+        return self._get_models(Student, self.complete_uri)
 
     def add_student(self, student):
         """
@@ -46,14 +28,7 @@ class StudentService():
         :param student: Student object to upload
         :type student: Student
         """
-        data = student._asdict()
-        del(data['id'])
-        response = requests.post(self.complete_uri, verify=self.verify, headers=self.headers, data=data)
-
-        if not (response.status_code >= 200 and response.status_code < 299):
-            response.raise_for_status()
-
-        return response
+        return self.add_many_students([student])
 
     def add_many_students(self, students):
         """
@@ -62,21 +37,4 @@ class StudentService():
         :param students: List of student objects
         :return:
         """
-
-        data = []
-
-        for student in students:
-            student = student._asdict()
-            del(student['id'])
-            data.append(student)
-
-        data = json.dumps(data)
-
-        headers = self.headers
-        headers['content-type'] = 'application/json'
-
-        response = requests.post(self.complete_uri, verify=self.verify, headers=headers, data=data)
-
-        response.raise_for_status()
-
-        return response
+        return self._add_many_models(students, self.complete_uri)
