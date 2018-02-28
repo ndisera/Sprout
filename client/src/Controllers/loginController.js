4fe0d25128389dffe278deabe7d1ac14e43e8770
Graham Zuber <gzuber@users.noreply.github.com>
2018-02-28 14:12:27 -0700

    alerts and toasts standardized. login error display fixed. (#226)
    * intermediate commit.
    
    * login errors fixed. alerts and toasts standardized.

--- client/src/Controllers/loginController.js
+++ client/src/Controllers/loginController.js
@@ -66,7 +66,8 @@ app.controller('loginController', function ($scope, $rootScope, $location, userS
         event.preventDefault();
 
         if(!validateInput($scope.email) || !validateInput($scope.password)) {
-            displayErrors("email and password must not be blank.");
+            displayErrors("Email and password must not be blank.");
+            return;
         }
 
         userService.login($scope.email, $scope.password).then(
@@ -75,6 +76,10 @@ app.controller('loginController', function ($scope, $rootScope, $location, userS
             }, function error(response) {
                 if (response.data && response.data["non_field_errors"]) {
                     displayErrors(response.data["non_field_errors"]);
+                }
+
+                if (response.data && response.data["email"]) {
+                    displayErrors(response.data["email"]);
                 }
             }
         );
