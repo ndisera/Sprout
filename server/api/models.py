a2ee40a3e1c9cdaa6ff57d01b7be1e19e0044f94
Simon Redman <simon@ergotech.com>
2018-03-04 12:43:48 -0700

    Dynamically generate notifications for student birthdays (#275)
    * Dynamically generate notifications for student birthdays
    
    * Add pretty-print for Student model
    
    * Add on_delete for Notification ForeignKeys
    
    * Add notification cleanup when a student's case manager changes
    
    * Cleanup notifications when an enrollment changes
    
    * Clean notifications between a teacher and student when an enrollment is deleted

--- server/api/models.py
+++ server/api/models.py
@@ -44,6 +44,12 @@ class Student(models.Model):
     class Meta:
         ordering = ('id',)
 
+    def __repr__(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+    def __str__(self):
+        return self.__repr__();
+
 
 class Holiday(models.Model):
     """
@@ -94,6 +100,19 @@ class Enrollment(models.Model):
     class Meta:
         unique_together = (('section', 'student'),)
         ordering = ('section',)
+
+    def delete(self, using=None, keep_parents=False):
+        """
+        Cleanup notifications between this enrollment's section's teacher
+        and the student if the enrollment is deleted
+        :return: 
+        """
+        old_student = self.student
+        old_teacher = self.section.teacher
+        old_notifications = Notification.objects.filter(student=old_student, user=old_teacher)
+        for notification in old_notifications:
+            notification.delete()
+        return super(Enrollment, self).delete(using, keep_parents)
 
 
 class Behavior(models.Model):
@@ -179,8 +198,8 @@ class Notification(models.Model):
     title = models.CharField(blank=False, max_length=settings.DEFAULT_MAX_CHARFIELD_LENGTH)
     body = models.CharField(blank=False, max_length=settings.DEFAULT_MAX_CHARFIELD_LENGTH)
     date = models.DateTimeField(blank=False)
-    student = models.ForeignKey(Student, blank=False)
-    user = models.ForeignKey(SproutUser, blank=False)
+    student = models.ForeignKey(Student, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(SproutUser, blank=False, on_delete=models.CASCADE)
     category = models.CharField(blank=False, max_length=settings.DEFAULT_MAX_CHARFIELD_LENGTH,
                                 help_text="Partial string of an API call, combined with student to create a URL from this notification")
     unread = models.BooleanField(blank=False, default=False)
--- server/api/serializers.py
+++ server/api/serializers.py
@@ -23,6 +23,20 @@ class StudentSerializer(DynamicModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        """
+        Clean up notifications for a case manager when a student is reassigned
+        """
+        if 'case_manager' in validated_data:
+            # If the case_manager is being updated, wipe all notifications
+            # between the student and the old case manager
+            old_case_manager = instance.case_manager
+            old_notifications = Notification.objects.filter(student=instance, user=old_case_manager)
+            for notification in old_notifications:
+                notification.delete()
+        return super(StudentSerializer, self).update(instance, validated_data)
+
     case_manager = DynamicRelationField('SproutUserSerializer')
     picture = DynamicRelationField('ProfilePictureSerializer')
 
@@ -84,6 +98,18 @@ class EnrollmentSerializer(DynamicModelSerializer):
     class Meta:
         model = Enrollment
         fields = ('id', 'section', 'student')
+
+    def update(self, instance, validated_data):
+        """
+        Clean up notifications for a teacher when a student's enrollment changes
+        """
+        old_student = instance.student
+        old_teacher = instance.section.teacher
+        old_notifications = Notification.objects.filter(student=old_student, user=old_teacher)
+        for notification in old_notifications:
+            notification.delete()
+        return super(EnrollmentSerializer, self).update(instance, validated_data)
+
     section = DynamicRelationField('SectionSerializer')
     student = DynamicRelationField('StudentSerializer')
 
