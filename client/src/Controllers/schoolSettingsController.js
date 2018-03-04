319fa18a80a791455b4bbf22a04d9a40e2829d15
Nico Disera <u0869073@utah.edu>
2018-03-04 16:34:48 -0700

    Added necessary service functions for settings page

--- client/src/Controllers/schoolSettingsController.js
+++ client/src/Controllers/schoolSettingsController.js
@@ -1,3 +1,8 @@
 app.controller("schoolSettingsController", function($scope, $rootScope, $location, toastService, userService) {
     $scope.location = $location;
+    $scope.terms = [];
+    $scope.holidays = [];
+    $scope.tests = [];
+
+    
 });
---
+++ client/src/Services/holidayService.js
@@ -0,0 +1,79 @@
+app.factory("holidayService", function ($rootScope, $http, $q, queryService) {
+    return {
+        /**
+         * Get holidays
+         * @param {object} config - config object for query parameters (see queryService)
+         * @return {promise} promise that will resolve with data or reject with response code.
+         */
+        getHolidays: function (config) {
+            var query = queryService.generateQuery(config);
+            var deferred = $q.defer();
+            $http({
+                method: 'GET',
+                url: 'https://' + $rootScope.backend + '/holidays' + query,
+            }).then(function success(response) {
+                deferred.resolve(response.data);
+            }, function error(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        },
+
+        /**
+         * Add a holiday
+         * @param {object} holidayObj - holiday to be added to terms.
+         * @return {promise} promise that will resolve with data or reject with response code.
+         */
+        addHoliday: function (holidayObj) {
+            var deferred = $q.defer();
+            $http({
+                method: 'POST',
+                url: 'https://' + $rootScope.backend + '/holidays',
+                data: holidayObj,
+            }).then(function success(response) {
+                deferred.resolve(response.data);
+            }, function error(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        },
+
+        /**
+         * Update holiday
+         * @param {number} holidayId - the holiday id.
+         * @param {object} holidayObj - the object used to update the holiday.
+         * @return {promise} promise that will resolve with data or reject with response code.
+         */
+        updateHoliday: function (holidayId, holidayObj) {
+            var deferred = $q.defer();
+            $http({
+                method: 'UPDATE',
+                url: 'https://' + $rootScope.backend + '/holidays/' + holidayId,
+                data: holidayObj,
+            }).then(function success(response) {
+                deferred.resolve(response.data);
+            }, function error(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        },
+
+        /**
+         * Delete holiday
+         * @param {number} holidayId - the holiday id.
+         * @return {promise} promise that will resolve with data or reject with response code.
+         */
+        deleteHoliday: function (holidayId) {
+            var deferred = $q.defer();
+            $http({
+                method: 'DELETE',
+                url: 'https://' + $rootScope.backend + '/holidays/' + holidayId,
+            }).then(function success(response) {
+                deferred.resolve(response.data);
+            }, function error(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        }
+    };
+});
