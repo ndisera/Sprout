5e9bde53d3ede1f3f0d9f25b3122495bf963f48d
Simon Redman <simon@ergotech.com>
2018-03-12 22:27:21 -0600

    Remove overridden to_representation for ProfilePictureSerializer

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -15,11 +15,6 @@ class ProfilePictureSerializer(DynamicModelSerializer):
         fields = '__all__'
         model = ProfilePicture
 
-    def to_representation(self, instance):
-        representation = super(ProfilePictureSerializer, self).to_representation(instance)
-        del (representation['file']) # Hide the file path from the response
-        return representation
-
 
 class StudentSerializer(DynamicModelSerializer):
     class Meta:
