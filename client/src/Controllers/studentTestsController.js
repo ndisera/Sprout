02e2bc5456f7069e73b7cdc256106f257c201ff6
Graham Zuber <grahamzuber@gmail.com>
2018-04-10 15:09:53 -0600

    fixed issue where labels were appearing off by a day on student test graphs.

--- client/src/Controllers/studentTestsController.js
+++ client/src/Controllers/studentTestsController.js
@@ -23,24 +23,7 @@ app.controller("studentTestsController", function ($scope, $rootScope, $location
     }
 
     // find biggest current term
-    $scope.selectedTerm = termService.getLargestCurrentTerm();
-    //_.each($scope.terms, function(elem) {
-        //if(moment() > elem.start_date && moment() < elem.end_date) {
-            //// I have found a candidate
-            //// but we want the biggest current term
-            //if($scope.selectedTerm === null) {
-                //$scope.selectedTerm = elem;
-            //}
-            //else {
-                //// take the bigger one
-                //var curDelta = $scope.selectedTerm.end_date - $scope.selectedTerm.start_date;
-                //var newDelta = elem.end_date - elem.start_date;
-                //if(newDelta > curDelta) {
-                    //$scope.selectedTerm = elem;
-                //}
-            //}
-        //}
-    //});
+    $scope.selectedTerm = termService.getLargestCurrentTerm($scope.terms);
 
     // set default date range to range of current term
     if($scope.selectedTerm !== null) {
@@ -135,7 +118,7 @@ app.controller("studentTestsController", function ($scope, $rootScope, $location
                 test.graph.data   = [];
                 test.graph.labels = [];
                 test.graph.data.push(_.times(dateDiff + 1, _.constant(null)));
-                test.graph.labels.push(_.times(dateDiff + 1, _.constant(null)));
+                test.graph.labels = _.times(dateDiff + 1, _.constant(null));
 
                 // iterate through each date, setting data as necessary
                 var iterDate = $scope.graphStartDate.clone();
