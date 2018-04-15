15e6559e44268e5edaa8e4e8307a33177346affd
Guy Watson <guy.w@me.com>
2018-04-15 16:31:24 -0600

    Change threshold for focus trend to 20%

--- server/focus_category/category_calculator.py
+++ server/focus_category/category_calculator.py
@@ -280,15 +280,15 @@ class CategoryCalculator():
                     slope_norm = model.params[1] * (y.last_valid_index() - y.first_valid_index()).days
 
                     # store only the most recent positive and negative examples
-                    # store the regression only if it passes a threshold: 15% slope (all our data is normalized)
-                    if slope_norm >= 0.15:
+                    # store the regression only if it passes a threshold: 20% slope (all our data is normalized)
+                    if slope_norm >= 0.20:
                         # only store one positive/negative example per data series
                         if positive_example is None:
                             positive_example = (slope_norm, curr_dataset,
                                                 y.first_valid_index(), y.last_valid_index(),
                                                 model.rsquared)
 
-                    if slope_norm <= -0.15:
+                    if slope_norm <= -0.20:
                         if negative_example is None:
                             negative_example = (slope_norm, curr_dataset,
                                                 y.first_valid_index(), y.last_valid_index(),
